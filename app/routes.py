44117563e88228977ec1aee525e47469768a3fa4
Abrar Attia <abrarattia@Abrars-MacBook-Pro.local>
2019-11-14 18:38:20 -0500

    added select room dropdown

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import Flask, render_template, request
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    rooms = ['ITB 137', 'ITB 139', 'ITB AB102']
+    return render_template('home.html', rooms=rooms)
