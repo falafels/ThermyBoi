0c49a6dab12c5ac255a77bdbda3767e27074da34
Jose Miguel Ballesteros <ballesjm@mcmaster.ca>
2020-02-28 14:57:41 -0500

    Connected UI to backend

--- app/server/logic/jobs/worker.py
+++ app/server/logic/jobs/worker.py
@@ -2,10 +2,13 @@ import time
 from .engine import Engine
 import datetime, threading, time
 
+intervalMins = 15
+
 def work():
     next_call = time.time()
     while True:
-        next_call = next_call+20
+        # next_call = next_call+(60*intervalMins)   PROD
+        next_call = next_call+20    # For Testing purposes
         time.sleep(next_call - time.time())
         Engine.execute_calculation()
 
--- app/server/routes/routes.py
+++ app/server/routes/routes.py
@@ -22,15 +22,3 @@ def getTemp():
 def postTemp():
     calc.add_temp(request.args.get("temp"))
     return jsonify({"success": True})
-
-@app.route('/getRooms') # take note of this decorator syntax, it's a common pattern
-def getRooms():
-    # It is good practice to only call a function in your route end-point,
-    # rather than have actual implementation code here.
-    # This allows for easier unit and integration testing of your functions.
-    return get_rooms()
-
-
-def get_rooms():
-    rooms = ['ITB 137', 'ITB 139', 'ITB AB102']
-    return json.dumps(rooms)
