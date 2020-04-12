bffe2372664b55662d86eef0d7a20ec3e956bd48
Jose Miguel Ballesteros <ballesjm@mcmaster.ca>
2020-04-12 16:29:42 -0400

    Fixed server issue

--- app/server/logic/jobs/worker.py
+++ app/server/logic/jobs/worker.py
@@ -11,9 +11,9 @@ def work():
             if os.environ['ENV'] == "production":
                 next_call = next_call+(60*intervalMins)
             else:
-                next_call = next_call+20
+                next_call = next_call+60
         else:
-            next_call = next_call+20        # DEBUG
+            next_call = next_call+60        # DEBUG
         time.sleep(next_call - time.time())
         Engine.execute_calculation()
 
